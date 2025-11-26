9fe611bd7231a32e52339e430640249cc87b254e
ADITYA BARANWAL <33594294+AdiBaranwal@users.noreply.github.com>
2025-11-26 17:25:52 +0530

    Create data.jsx

---
+++ data.jsx
@@ -0,0 +1,18 @@
+const explorerData = {
+  name: 'src',
+  isFolder: true,
+  children: [
+    {
+      name: 'components',
+      isFolder: true,
+      children: [
+        { name: 'Header.js', isFolder: false },
+        { name: 'Footer.js', isFolder: false },
+      ],
+    },
+    { name: 'App.js', isFolder: false },
+    { name: 'index.js', isFolder: false },
+  ],
+};
+
+export default explorerData;
